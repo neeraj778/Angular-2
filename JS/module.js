84c7debb0c1dbae9e6c97324699730ebb176a468
neeraj778 <neeraj.778@gmail.com>
2017-03-04 23:07:26 +0530

    Update module.js

--- JS/module.js
+++ JS/module.js
@@ -1,8 +1,1 @@
-angular.module('app',['ngRoute'])
-        .config('routeProvider',function($routeProvider){
-          routeProvider.when('/',function(){
-            templateUrl: 'partials/page1.html',
-            controller: 'page1Controller'
-          })
-        
-        })
+angular.module('app',['ngRoute','page1Controller'])
