c38cf93b0a8a35dd11242730a4efbe2d9148a47b
neeraj778 <neeraj.778@gmail.com>
2017-03-06 10:02:55 +0530

    Update controllers.js

--- JS/controllers.js
+++ JS/controllers.js
@@ -1,6 +1,6 @@
 angular.module('app',[])
        .controller('page1Controller',function($scope,$http){
-          $http.get('https://www.w3schools.com/angular/welcome.htm').then(function(resp){
+          $http.get('Data/textFile.txt').then(function(resp){
               $scope.names = resp.data;
           })
        });
