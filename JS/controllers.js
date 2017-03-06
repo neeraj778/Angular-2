75cda58275a23e122d032bd46f28d95f10997c60
neeraj778 <neeraj.778@gmail.com>
2017-03-06 10:42:10 +0530

    Update controllers.js

--- JS/controllers.js
+++ JS/controllers.js
@@ -1,4 +1,4 @@
-angular.module('controllers',[])
+angular.module('controllers')
        .controller('page1Controller',function($scope,$http){
           $http.get('Data/names.json').success(function(data){
               $scope.names = data;
