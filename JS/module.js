6d569e2f12a2325830317809287f295c7d2b1eb7
neeraj778 <neeraj.778@gmail.com>
2017-03-06 11:00:15 +0530

    Update module.js

--- JS/module.js
+++ JS/module.js
@@ -1,14 +1,1 @@
-angular.module('app',[])
-        .controller('page1Controller',function($scope,$http){
-                    $scope.names2 = [
-                                    {name: 'Neeraj', age: 28, sex: 'male'},
-                                    {name: 'Ankit', age: 26, sex: 'male'},
-                                    {name: 'Deepti', age: 30, sex: 'female'}
-                                  ];
-
-                    /*
-                    $http.get('Data/names.json').then(function(data){
-                           $scope.names = data;
-                    });
-                    */
-        });
+angular.module('app',['controllers'])
