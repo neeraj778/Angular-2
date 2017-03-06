9fa2b01ce30f5c2e3df0f98ddf522e418edbba35
neeraj778 <neeraj.778@gmail.com>
2017-03-06 10:23:56 +0530

    Update controllers.js

--- JS/controllers.js
+++ JS/controllers.js
@@ -1,5 +1,5 @@
        app.controller('page1Controller',function($scope,$http){
-              $scope.names = [
+              $scope.names2 = [
                               {name: 'Neeraj', age: 28, sex: 'male'},
                               {name: 'Ankit', age: 26, sex: 'male'},
                               {name: 'Deepti', age: 30, sex: 'female'}
